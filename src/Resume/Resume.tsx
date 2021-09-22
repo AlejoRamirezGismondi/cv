45f424792ca77cb7adfc5f46bb5979be0c787ac6
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-09-22 18:58:23 -0300

    added material

--- src/Resume/Resume.tsx
+++ src/Resume/Resume.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {Paper} from "@mui/material";
 
 const Resume = () => {
-  return <div/>
+  return (
+    <div className={'root'}>
+      {/*<div className={'header'}>*/}
+      {/*  <h1>Alejo Ramirez Gismondi</h1>*/}
+      {/*</div>*/}
+      <div className={'content'}>
+        <Paper className={'left-bar'} elevation={3}>
+          <p>text</p>
+        </Paper>
+        <div className={'right-content'}>
+          <p>text</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Resume;
