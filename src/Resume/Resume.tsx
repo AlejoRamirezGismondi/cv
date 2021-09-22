bb0f581cc0d8f1964280cf4b2c5ee56ab7bd4fa5
AlejoRamirezGismondi <alejo.ramirezgismondi@ing.austral.edu.ar>
2021-09-22 19:36:00 -0300

    education section

--- src/Resume/Resume.tsx
+++ src/Resume/Resume.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
+import './Resume.css';
 import {Paper} from "@mui/material";
 
 const Resume = () => {
   return (
     <div className={'root'}>
-      {/*<div className={'header'}>*/}
-      {/*  <h1>Alejo Ramirez Gismondi</h1>*/}
-      {/*</div>*/}
       <div className={'content'}>
-        <Paper className={'left-bar'} elevation={3}>
-          <p>text</p>
-        </Paper>
+        <div className={'left-content'}>
+          <Paper className={'left-paper'} elevation={4}>
+            <h1>Alejo Ramírez Gismondi</h1>
+            <h3>Software Engineer</h3>
+            <div>
+              <h2>Contact</h2>
+              <p>ramirezgismondialejo@gmail.com</p>
+              <p>No socials for now :D</p>
+            </div>
+          </Paper>
+        </div>
         <div className={'right-content'}>
-          <p>text</p>
+          <div className={'education'}>
+            <h4>Education</h4>
+            <ul>
+              <li><a href={'https://holycross.ar/'}>Holy Cross Boys School</a></li>
+              <li><a href={'https://www.austral.edu.ar/ingresantes/carreras/ingenieria-en-informatica/'}>Universidad Austral:</a> Ingeniería
+                Informática (4th out of 5 years)</li>
+              <li>IELTS Academic: Overall Band 8.5</li>
+              <div>
+                <p>Online Courses</p>
+                <ul>
+                  <li>Bootstrap 4: Universidad Austral</li>
+                  <li>Introduction to Cyber Attacks: NYU</li>
+                  <li>Cyber Attacks Countermeasures: NYU</li>
+                  <li>Others</li>
+                </ul>
+              </div>
+            </ul>
+          </div>
+          <div className={'technical-skills'}>
+            <h4>Technical Skills</h4>
+          </div>
+          <div>skills</div>
+          <div>work experience</div>
         </div>
       </div>
     </div>
